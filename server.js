33c0a0b9dd1c0632f1c31694167f0b48991b8344
Anthonyshakurangelos <anthonyshakurangelos@gmail.com>
2023-02-08 20:05:04 -0700

    got server running

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
+// const { hasSubscribers } = require('diagnostics_channel');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
