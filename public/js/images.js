be51dcff4b81f50593fdbc352ff01a3ffb43e362
Kate Steed <sunfiresteed@yahoo.com>
2023-02-15 19:39:15 -0700

    stashing

--- public/js/images.js
+++ public/js/images.js
@@ -4,9 +4,15 @@ uploadPreset: 'of5kcle0',
 folder: 't-party-photos'}, (error, result) => { 
     if (!error && result && result.event === "success") { 
     console.log('Done! Here is the image info: ', result.info); 
+    
+    console.log(result.info.public_id)
+
     }
-}
-)
+})
+
+//function uploadPhoto(result) {
+//    const photo = result.public_id
+//}
 
 document.getElementById("upload_widget").addEventListener("click", function(){
     myWidget.open();
